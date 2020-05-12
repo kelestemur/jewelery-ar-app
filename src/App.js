2a1c84c1995b86ea6bcc81b02e7af52b269d8ec2
keletemur <keletemur@gmail.com>
2020-05-11 22:47:02 -0500

    some changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ExamplePage from './ExamplePage'
+import ProxyPage from './Pages/ProxyPage'
+import {
+  HashRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
+    <HashRouter basename="/">
     <div className="App">
+      <Switch>
+        <Route path = "/proxy">
+      <ProxyPage/>
+      </Route>
+      <Route path = "/">
       <ExamplePage> </ExamplePage>
+      </Route>
+      </Switch>
     </div>
+    </HashRouter>
   );
 }
 
---
+++ src/Pages/ProxyPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ProxyPage() {
+    var repsonse = {
+        "Content-Type":'application/liquid',
+        "Status": 200
+        
+    }
+    return (
+        <div>
+            proxy page
+        </div>
+    );
+}
+
+export default ProxyPage;
